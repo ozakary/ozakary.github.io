56eec08ab2de6deca583254cc540616accdedc0f
Ouail Zakary <zakaryouail@gmail.com>
2025-03-01 09:52:20 +0200

    Fix background image path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/ozakary.github.io/',  // Change this to match your repository name
 })
