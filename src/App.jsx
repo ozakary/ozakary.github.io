6cfba44790b634697c760008c58e803929fc469e
ozakary <zakaryouail@gmail.com>
2025-06-06 13:54:02 +0300

    added conference, code, and some news

--- src/App.jsx
+++ src/App.jsx
@@ -165,10 +165,16 @@ const App = () => {
     
     const news = [
       {
+        date: "June 2025",
+        title: "Poster presentation",
+        content: "Presented a poster titled \"<b>Local E(3)-Equivariant Neural Network Force Field for Modeling Host-Guest Interactions in Xenon-Based Porous Organic Cages</b>\" at the <i><b>Computational Chemistry Days 2025</b></i> conference, organized at the <i><b>Department of Chemistry and Materials Science</b></i> at <i><b>Aalto University</b></i>.",    
+        link: "https://ocamm.fi/event/computational-chemistry-days-2025/"
+      },
+      {
         date: "March 2025",
         title: "Poster presentation",
         content: "Presented a poster titled \"<b>Machine Learning-Driven Approach for Modeling Host-Guest Dynamics in Xenon-Based Porous Liquids</b>\" at the <i><b>Physics Days 2025</b></i> conference, organized by the <i><b>University of Oulu</b></i>, where our research unit was part of the organizing committee.",    
-        link: "https://doi.org/10.6084/m9.figshare.28684814.v1"
+        link: "https://ssl.eventilla.com/physicsdays2025"
       },
       {
         date: "March 2025",
@@ -773,6 +779,19 @@ const App = () => {
     const conferences = [
       {
         type: CONFERENCE_TYPES.POSTER,
+        titleHtml: "Local E(3)-Equivariant Neural Network Force Field for Modeling Host-Guest Interactions in Xenon-Based Porous Organic Cages",
+        authors: [
+          { name: "Ouail Zakary", isPI: true },
+          { name: "Perttu Lantto" }
+        ],
+        conferenceNameHtml: "Computational Chemistry Days 2025",
+        location: "Espoo, Finland",
+        date: "02-03/06/2025",
+        abstractHtml: "Porous organic cages (POCs) in porous liquids (PLs) offer promising platforms for selective Xe capture. Understanding Xe binding and dynamics in these systems is crucial for material design. While DFT-based molecular dynamics (MD) accurately models these interactions, it is computationally prohibitive for large systems. Machine learning interatomic potentials (MLIPs), particularly neural networks like Allegro, provide a scalable alternative. We present an MLIP trained on DFT-D4 theory-level data for PLs and POCs with Xe, covering over 1.8 million atoms across varied structures. Furthermore, we train an invariant SchNet model to predict <sup>129</sup>Xe NMR magnetic shielding tensors, offering microscopic insights into static NMR spectra and dynamic relaxation behaviors.",
+        doi: "https://doi.org/10.6084/m9.figshare.28684814.v1"
+      },
+      {
+        type: CONFERENCE_TYPES.POSTER,
         titleHtml: "Machine Learning Driven Approach for Modeling Host-Guest Dynamics in Xenon-Based Porous Liquids",
         authors: [
           { name: "Ouail Zakary", isPI: true },
@@ -781,7 +800,7 @@ const App = () => {
         conferenceNameHtml: "Physics Days 2025",
         location: "Oulu, Finland",
         date: "26-28/03/2025",
-        abstractHtml: "Porous materials serve diverse applications like molecular separations and catalysis, offering an energy-efficient method for capturing greenhouse gases (CO2, and CH4 ) and valuable noble gases (Xe, Ar, and Kr). Xenon — vital in optics, medicine, and nuclear fission processes — poses extraction challenges due to its low atmospheric abundance (0.087 ppm by volume) and inertness, driving high commercial costs. Porous materials must exhibit precise size selectivity and high adsorption capacity for effective xenon isolation in commercial applications. The recent development of porous liquids (PLs) with cavities formed by porous organic cages (POCs) has shown promise in addressing these challenges. Understanding the binding, occupancies, dynamics, and equilibrium between the host (PL and POC) and the guest (Xe) is vital in engineering new POCs with targeted functionalities. Molecular dynamics (MD) simulations have proven essential for understanding and exploring the physicochemical processes governing these systems. In MD simulations, atom movements are governed by forces derived as gradients of the potential energy surface (PES) of the system. Typically, the PES is accurately obtained by calculating the electronic structure using methods like density functional theory (DFT). While this combination of MD with DFT offers precise interatomic forces, their computational scaling limits simulations to tens of picoseconds and a few hundred atoms, which is far from capturing realistic timescales and size of these porous systems. <br> Classical models for computing PES, although faster, lack accuracy. In recent years, machine learning (ML), particularly neural networks (NNs), has emerged as a promising solution to these limitations by learning accurate interatomic potentials from a set of high-fidelity ab initio reference calculations while maintaining computational efficiency. <br> Here, we present a machine learning interatomic potential (MLIP) model constructed using the local equivariant deep NN architecture, Allegro. This model was trained, validated, and tested on energies, forces, and virials — computed at the DFT-D4 level with PBE functional and periodic boundary conditions — of structures issued from semi-empirical MD simulations performed at RT and 600K, from a dataset comprising over 1600 structures consisting of 600 to 1170 atoms (H, C, N, O, F, Cl, and Xe). These structures comprise one to two xenon atoms in TBA-type, HAP-type, and DCT-type PLs, as well as CC3 POC where Xe atoms are positioned inside or near the cavities of the POC. The dataset encompasses 1.8 million atoms, with 12.5 million data points used in the process. <br> The MLIP will be applied to provide microscopic interpretation of experimental 129Xe NMR observations. In this step, we will construct an ML model for magnetic shielding tensors, obtained from the dataset. This ML model will later be used to predict the magnetic shielding in MD simulations to explain experimental, both static spectral and dynamic relaxation, data.",
+        abstractHtml: "Porous materials serve diverse applications like molecular separations and catalysis, offering an energy-efficient method for capturing greenhouse gases (CO<sub>2</sub>, and CH<sub>4</sub> ) and valuable noble gases (Xe, Ar, and Kr). Xenon — vital in optics, medicine, and nuclear fission processes — poses extraction challenges due to its low atmospheric abundance (0.087 ppm by volume) and inertness, driving high commercial costs. Porous materials must exhibit precise size selectivity and high adsorption capacity for effective xenon isolation in commercial applications. The recent development of porous liquids (PLs) with cavities formed by porous organic cages (POCs) has shown promise in addressing these challenges. Understanding the binding, occupancies, dynamics, and equilibrium between the host (PL and POC) and the guest (Xe) is vital in engineering new POCs with targeted functionalities. Molecular dynamics (MD) simulations have proven essential for understanding and exploring the physicochemical processes governing these systems. In MD simulations, atom movements are governed by forces derived as gradients of the potential energy surface (PES) of the system. Typically, the PES is accurately obtained by calculating the electronic structure using methods like density functional theory (DFT). While this combination of MD with DFT offers precise interatomic forces, their computational scaling limits simulations to tens of picoseconds and a few hundred atoms, which is far from capturing realistic timescales and size of these porous systems. <br> Classical models for computing PES, although faster, lack accuracy. In recent years, machine learning (ML), particularly neural networks (NNs), has emerged as a promising solution to these limitations by learning accurate interatomic potentials from a set of high-fidelity ab initio reference calculations while maintaining computational efficiency. <br> Here, we present a machine learning interatomic potential (MLIP) model constructed using the local equivariant deep NN architecture, Allegro. This model was trained, validated, and tested on energies, forces, and virials — computed at the DFT-D4 level with PBE functional and periodic boundary conditions — of structures issued from semi-empirical MD simulations performed at RT and 600K, from a dataset comprising over 1600 structures consisting of 600 to 1170 atoms (H, C, N, O, F, Cl, and Xe). These structures comprise one to two xenon atoms in TBA-type, HAP-type, and DCT-type PLs, as well as CC3 POC where Xe atoms are positioned inside or near the cavities of the POC. The dataset encompasses 1.8 million atoms, with 12.5 million data points used in the process. <br> The MLIP will be applied to provide microscopic interpretation of experimental 129Xe NMR observations. In this step, we will construct an ML model for magnetic shielding tensors, obtained from the dataset. This ML model will later be used to predict the magnetic shielding in MD simulations to explain experimental, both static spectral and dynamic relaxation, data.",
         doi: "https://doi.org/10.6084/m9.figshare.28684814.v1"
       },
       {
@@ -1033,6 +1052,13 @@ const App = () => {
 
   const Code = () => {
     const projects = [
+      {
+        titleHtml: "TrajSlicer",
+        descriptionHtml: "A versatile Python tool for converting LAMMPS dump files to XYZ format and sampling molecular dynamics trajectories with precise frame control.",
+        technologies: ["Python"],
+        github: "https://github.com/ozakary/TrajSlicer",
+        type: "Research Code & Data Analysis"
+      },      
       {
         titleHtml: "NMR-VASP",
         descriptionHtml: "The VASP NMR Parameter Calculator extracts and processes NMR data from VASP output files (OUTCAR) that the standard VASP processing doesn't provide directly. This tool is particularly useful for researchers working with NMR computations in material science.",
